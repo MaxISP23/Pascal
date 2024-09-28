51a2f35ecde561001e47d64f012d4ae3272f3db4
MaxISP23 <maxbogomol159@gmail.com>
2024-09-28 10:56:25 +0500

    Update 6.pas

--- 6.pas
+++ 6.pas
@@ -5,6 +5,18 @@ var a, b, c, mid : real;
 begin
    write('Введите a, b, c: ');
    read(a, b, c);
-   mid := (a + b + c) / 3;
-   writeln('Среднее число: ', mid);
+   if ((a = b) or (b = c) or (a = c)) then begin
+     writeln('Нельзя вычилить среднее число!');
+   end else begin
+     if (((a > b) and (a < c)) or ((a < b) and (a > c))) then begin
+       mid := a;
+     end else begin
+       if (((b > a) and (b < c)) or ((b < a) and (b > c))) then begin
+         mid := b;  
+       end else begin
+         mid := c;
+       end;
+     end;
+     writeln('Среднее число: ', mid);
+   end;
 end.
