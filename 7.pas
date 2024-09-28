2769c8781f5e7369aa999803eed481eb0606784e
MaxISP23 <maxbogomol159@gmail.com>
2024-09-28 10:56:58 +0500

    Update 7.pas

--- 7.pas
+++ 7.pas
@@ -6,9 +6,9 @@ begin
    write('Введите a, b, c: ');
    read(a, b, c);
    
-   if ((a <> 0) and (b <> 0) and (a <> 0)) then begin
-     writeln('Среднее число: ', (a + b + c) / 3);
-   end else begin
+   if ((a = 0) or (b = 0) or (a = 0)) then begin
      writeln('Числа равны нулю');
+   end else begin
+     writeln('Среднее арифметическое число: ', (a + b + c) / 3);
    end;
 end.
