266f8dfed2f968fbe5e801bb8afc650f6e6dad5f
MaxISP23 <maxbogomol159@gmail.com>
2024-09-30 13:48:10 +0500

    Update 14.pas

--- 14.pas
+++ 14.pas
@@ -1,12 +1,15 @@
 program work14;
 
 var 
-  a : integer;
+  a, b, c, max : integer;
 
 begin
-   write('Угол: ');
-   read(a);
-   case a of
+   write('Введите три угла треугольника: ');
+   read(a, b, c);
+   max := a;
+   if (max < b) then max := b;
+   if (max < c) then max := c;
+   case max of
      90 : writeln('Прямой');
      1..89 : writeln('Острый');
      91..179 : writeln('Тупой');
