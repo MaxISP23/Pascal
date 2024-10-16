79aaa1dee28ac9ad4cdec583d7bbba9f786f3981
MaxISP23 <maxbogomol159@gmail.com>
2024-10-16 22:52:21 +0500

    Update 29.pas

--- 29.pas
+++ 29.pas
@@ -3,13 +3,14 @@ program work29;
 var
   a: string;
   upper, lower, i: integer;
+  
 begin
   write('Введите строку: ');
   read(a);
 
   for i := 1 to length(a) do begin
-    if (a[i] in ['А'..'Я']) or (a[i] in ['A'..'Z']) then inc(upper);
-    if (a[i] in ['а'..'я']) or (a[i] in ['a'..'z']) then inc(lower);
+    if (a[i] in ['А'..'Я']) or (a[i] in ['A'..'Z']) then upper := upper + 1;
+    if (a[i] in ['а'..'я']) or (a[i] in ['a'..'z']) then lower := lower + 1;
   end;
 
   writeln('Заглавных букв в строке: ', upper);
