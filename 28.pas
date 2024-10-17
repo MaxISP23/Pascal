290088412d52bbb1cb5c7aa62f9b60f0d7d38748
MaxISP23 <maxbogomol159@gmail.com>
2024-10-17 13:09:08 +0500

    Update 28.pas

--- 28.pas
+++ 28.pas
@@ -1,14 +1,28 @@
 program work28;
 
-var a : string;
+var 
+  a : string;
+  b : integer;
 
 begin
   write('Введите строку: ');
   read(a);
   while pos('  ', a) > 0 do begin
     delete(a, pos('  ', a), 1);
+    b := b + 1;
   end;
-  if (a[1] = ' ') then delete(a, 1, 1); 
-  if (a[length(a)] = ' ') then delete(a, length(a), 1); 
-  write(a);
+  if (length(a) > 0) then begin 
+    if (a[1] = ' ') then begin 
+      delete(a, 1, 1);
+      b := b + 1;
+    end;
+  end;
+  if (length(a) > 0) then begin 
+    if (a[length(a)] = ' ') then begin 
+      delete(a, length(a), 1); 
+      b := b + 1;
+    end;
+  end;
+  writeln(a);
+  writeln('Число удалённых пробелов: ', b);
 end.
