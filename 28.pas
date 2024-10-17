bc6c25b24c2f407d19e2182a68d2ff40d4fdad51
MaxISP23 <maxbogomol159@gmail.com>
2024-10-17 13:00:59 +0500

    Update 28.pas

--- 28.pas
+++ 28.pas
@@ -5,8 +5,10 @@ var a : string;
 begin
   write('Введите строку: ');
   read(a);
-  while pos('  ', a)>0 do begin
-    delete(a, pos('  ', a),1);
+  while pos('  ', a) > 0 do begin
+    delete(a, pos('  ', a), 1);
   end;
+  if (a[1] = ' ') then delete(a, 1, 1); 
+  if (a[length(a)] = ' ') then delete(a, length(a), 1); 
   write(a);
 end.
